41dea302d701ef77276193a3a681edae509b862d
Nelio Memeje <neliomemeje@yahoo.com>
2023-11-13 16:17:04 +0800

    adding styles

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const logoIcon = document.querySelector(".logo-container");
 const sidebar = document.querySelector(".hidden-sidebar");
 const searchBox = document.querySelector(".search-box");
 const input = document.querySelector(".search-box input");
-const searchIcon = document.querySelector(".search-box .search-icon");
 const div = document.querySelector(".blur");
+const nav = document.querySelector("nav");
 
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("show-hide");
@@ -16,11 +16,14 @@ div.addEventListener("click", () => {
   sidebar.classList.toggle("show-hide");
   div.classList.toggle("show");
 });
-searchIcon.addEventListener("click", () => {
-  menuBtn.classList.add("hide");
-  logoIcon.classList.add("hide");
-  input.classList.add("display");
-  arrowLeft.classList.add("display");
+
+searchBox.addEventListener("click", () => {
+  if (searchBox.offsetWidth < 100) {
+    menuBtn.classList.add("hide");
+    logoIcon.classList.add("hide");
+    input.classList.add("display");
+    arrowLeft.classList.add("display");
+  }
 });
 arrowLeft.addEventListener("click", () => {
   menuBtn.classList.remove("hide");
@@ -28,15 +31,12 @@ arrowLeft.addEventListener("click", () => {
   input.classList.remove("display");
   arrowLeft.classList.remove("display");
 });
-function reportWindowSize() {
-  if (window.innerWidth > 650) {
+
+window.addEventListener("resize", () => {
+  if (nav.offsetWidth < 600) {
+    input.classList.remove("display");
     arrowLeft.classList.remove("display");
     menuBtn.classList.remove("hide");
     logoIcon.classList.remove("hide");
   }
-  if (window.innerWidth < 600) {
-    input.classList.remove("display");
-  }
-}
-
-window.onresize = reportWindowSize;
+});
